5aa5c5b066c69a15337aa50160f2049ae7d8b7e0
theart84 <artshadow74@gmail.com>
2021-10-22 09:47:15 +0500

    added swagger

--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -1,21 +1,26 @@
 import {Type} from 'class-transformer';
 import {IsArray, IsEnum, IsNumber, IsObject, IsString, Length, ValidateNested} from 'class-validator'
+import {ApiProperty} from "@nestjs/swagger";
 
 export class DescriptionDetailDto {
+  @ApiProperty()
   @IsString()
   @Length(5, 25)
   color: string;
 
+  @ApiProperty()
   @IsString()
   details: string
 }
 
 export class DescriptionDto {
+  @ApiProperty()
   @IsObject()
   @ValidateNested()
   @Type(() => DescriptionDetailDto)
   ru: DescriptionDetailDto;
 
+  @ApiProperty()
   @IsObject()
   @ValidateNested()
   @Type(() => DescriptionDetailDto)
@@ -30,21 +35,26 @@ export enum TypeProduct {
 
 
 export class CreateProductDto {
+  @ApiProperty()
   @IsString()
   @Length(5, 25)
   title: string;
 
+  @ApiProperty()
   @IsObject()
   @ValidateNested()
   @Type(() => DescriptionDto)
   description: DescriptionDto;
 
+  @ApiProperty()
   @IsNumber()
   price: number;
 
+  @ApiProperty({ enum: Object.keys(TypeProduct).filter(k => typeof TypeProduct[k as any] !== "number") })
   @IsEnum(TypeProduct)
   type: TypeProduct;
 
+  @ApiProperty()
   @IsArray()
   @IsString({each: true})
   categories: string[];
